313273d92e845a494ce6c4ff3c8b1b02a244a79c
jack-trevaskis <jack.trevaskis1@gmail.com>
2024-09-15 03:03:45 +1200

    all drop down bars working

--- script.js
+++ script.js
@@ -1,75 +1,53 @@
-//detect when button is clicked
-//function
 
 
-
-// keeps track of img index number
+// -------------- CHANGE BACKGROUND IMAGE ----------------
 
 let imgIndex = 0
- document.getElementById("body").classList.add("backgroundIMG1")
+
+let IMG = document.getElementById('body')
+
+document.getElementById("body").classList.add("backgroundIMG1")
 
 
 
-function imgIdTracker() {
+function changeBackground() {
   
   if (imgIndex < 1) {
+
     imgIndex++
-  }
-  else {
-    imgIndex = 0
-  }
-
-  
-  changeBackground ()
-}
-
-
-
-// uses img index to change class
-
-function changeBackground () {
-
-  let IMG = document.getElementById('body')
-
-  if (imgIndex < 1) {
-
-    IMG.classList.remove("backgroundIMG2")
-
-    IMG.classList.add('backgroundIMG1')
-
-    document.getElementById("button").innerHTML = "JAcK SHREDZ ON GUITAR????"
-
-  } 
-
-  else {
 
     IMG.classList.remove("backgroundIMG1")
 
-    IMG.classList.add("backgroundIMG2")
+    IMG.classList.add('backgroundIMG2')
+
+    document.getElementById("button").innerHTML = "JAcK SHREDZ ON GUITAR????"
+  }
+  else {
+
+    imgIndex = 0
+
+    IMG.classList.remove("backgroundIMG2")
+
+    IMG.classList.add("backgroundIMG1")
 
     document.getElementById("button").innerHTML = "JACK SHREDZ ON SKIS????"
-
   }
-
-   console.log(imgIndex)
 
 }
 
-document.getElementById("button").onclick = imgIdTracker
+document.getElementById("button").onclick = changeBackground
 
 
 
-// Drop down menu
+// --------------- DROP DOWN MENUS --------------------
 
+
+// BLOGS
 
 
 const blogs = document.getElementById("blogs");
-const blogDrop = document.getElementById("blogsDrop") 
 
-
-// blogs.addEventListener("click", blogsUp)
-
-// blog drop down memory
+const blogDrop = document.getElementById("blogsDrop")
 
 let blogsUpDown = 0
 
@@ -77,16 +55,80 @@ let blogsUpDown = 0
 function blogsDrop() {
   
     if (blogsUpDown < 1) {
-      blogDrop.classList.remove("blogsDrop")
-      blogDrop.classList.add("blogsUp")
+      blogDrop.classList.remove("blogsUp")
+      blogDrop.classList.add("blogsDrop")
       blogsUpDown++
     }
     else {
-      blogDrop.classList.remove("blogsUp")
-      blogDrop.classList.add("blogsDrop")
+    
+      blogDrop.classList.remove("blogsDrop")
+      blogDrop.classList.add("blogsUp")
       blogsUpDown = 0
     }
   
 }
 
 blogs.addEventListener("click", blogsDrop)
+
+
+
+// PERSONAL
+
+
+const personal = document.getElementById("personal");
+
+const personalDrop = document.getElementById("personalDrop")
+
+let personalUpDown = 0
+
+
+function personalsDrop() {
+  
+    if (personalUpDown < 1) {
+      
+      personalDrop.classList.remove("personalUp")
+      personalDrop.classList.add("personalDrop")
+      personalUpDown++
+    }
+    else {
+      
+      personalDrop.classList.remove("personalDrop")
+      personalDrop.classList.add("personalUp")
+      personalUpDown = 0
+    }
+  
+}
+
+personal.addEventListener("click", personalsDrop)
+
+
+// DA TASKS
+
+
+const DA = document.getElementById("DA");
+
+const DADrop = document.getElementById("DADrop")
+
+let DAUpDown = 0
+
+
+function DAsDrop() {
+  
+    if (DAUpDown < 1) {
+      
+      DADrop.classList.remove("DAUp")
+      DADrop.classList.add("DADrop")
+      DAUpDown++
+    }
+    else {
+      
+      DADrop.classList.remove("DADrop")
+      DADrop.classList.add("DAUp")
+      DAUpDown = 0
+    }
+  
+}
+
+DA.addEventListener("click", DAsDrop)
+
+
