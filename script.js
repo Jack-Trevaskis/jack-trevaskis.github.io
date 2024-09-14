499cc17d176cb0807a85f9c603f664be2dd48aae
jack-trevaskis <jack.trevaskis1@gmail.com>
2024-09-14 23:50:48 +1200

    add change background image button

---
+++ script.js
@@ -0,0 +1,61 @@
+//detect when button is clicked
+//function
+
+
+
+// keeps track of img index number
+
+let imgIndex = 0
+ document.getElementById("body").classList.add("backgroundIMG1")
+
+
+
+function imgIdTracker() {
+  
+  if (imgIndex < 1) {
+    imgIndex++
+  }
+  else {
+    imgIndex = 0
+  }
+
+  
+  changeBackground ()
+}
+
+
+
+// uses img index to change class
+
+function changeBackground () {
+
+  let IMG = document.getElementById('body')
+
+  if (imgIndex < 1) {
+
+    IMG.classList.remove("backgroundIMG2")
+
+    IMG.classList.add('backgroundIMG1')
+
+    document.getElementById("button").innerHTML = "JAcK SHREDZ ON GUITAR????"
+
+  } 
+
+  else {
+
+    IMG.classList.remove("backgroundIMG1")
+
+    IMG.classList.add("backgroundIMG2")
+
+    document.getElementById("button").innerHTML = "JACK SHREDZ ON SKIS????"
+
+  }
+
+   console.log(imgIndex)
+
+}
+
+document.getElementById("button").onclick = imgIdTracker
+
+
+
